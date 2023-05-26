d56ad89780574fc46a0309346a5d2a563beabd07
LadyDementor <125830907+LadyDementor@users.noreply.github.com>
2023-05-26 11:39:46 +0300

    initial setup & migration

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,6 +3,7 @@ const knex = require('knex');
 const configurations = require('../knexfile.js');
 const environment = process.env.NODE_ENV || 'development';
 
+
 // Gerçekte hangi knex yapılandırması kullanılıyor?
 // Bu, process.env.NODE_ENV'nin değerine bağlıdır!
 module.exports = knex(configurations[environment]);
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Server'ı buradan başlatın
+
+
+const port=9000;
+const server=require("./api/server");
+
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
